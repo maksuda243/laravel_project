0d138b5f5134f90c92b7abfd538ac66578c4934c
mojahidjouel <mojahidjouel95@gmail.com>
2023-12-10 22:31:33 -0800

    git ignore update

--- resources/views/employeruser/layout/app.blade.php
+++ resources/views/employeruser/layout/app.blade.php
@@ -154,7 +154,7 @@
               <div class="menu_section">
                 <h3>General</h3>
                 <ul class="nav side-menu">
-                  <li><a href="{{route('jobpost.create')}}"><i class="fa fa-home"></i> Post a Job </a>
+                  <li><a href="{{route('job_post.create')}}"><i class="fa fa-home"></i> Post a Job </a>
                     
                   </li>
                   <li><a><i class="fa fa-edit"></i> Forms <span class="fa fa-chevron-down"></span></a>
