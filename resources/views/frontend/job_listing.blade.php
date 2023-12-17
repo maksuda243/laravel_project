65243ce6b5799b8bf19a1abd0160041c1b7f82c9
Maksuda Akter <maksudaakter243@gmail.com>
2023-12-18 02:03:42 +0600

    updated

--- resources/views/frontend/job_listing.blade.php
+++ resources/views/frontend/job_listing.blade.php
@@ -106,6 +106,17 @@
                                     <div class="col-lg-12">
                                         <div class="count-job mb-35">
                                             <span>39, 782 Jobs found</span>
+                                            <!-- Select job items start 
+                                            <div class="select-job-items">
+                                                <span>Sort by</span>
+                                                <select name="select">
+                                                    <option value="">None</option>
+                                                    <option value="">job list</option>
+                                                    <option value="">job list</option>
+                                                    <option value="">job list</option>
+                                                </select>
+                                            </div>
+                                            <!--  Select job items End-->
                                         </div>
                                     </div>
                                 </div>
@@ -122,8 +133,8 @@
                                                     <h4>{{$job->job_title}}</h4>
                                                 </a>
                                                 <ul>
-                                                    <li>{{$job->company_name}}</li>
-                                                    <li><i class="fas fa-map-marker-alt"></i>{{$job->location}}</li>
+                                                    <li>{{$job->employer?->company_name}}</li>
+                                                    <li><i class="fas fa-map-marker-alt"></i>{{$job->employer?->address}}</li>
                                                     <li>{{$job->salary}}</li>
                                                 </ul>
                                             </div>
