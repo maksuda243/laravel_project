3ff4db124012c055c22e24ba3be6ab69feee6c66
ishtiaktauhid <tauhidulislam1516@gmail.com>
2024-01-01 22:26:59 -0800

    updated

--- routes/api.php
+++ routes/api.php
@@ -19,4 +19,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('blogs/{cat}',[BlogsApiController::class,'index']);
+Route::get('blogs/{id}',[BlogsApiController::class,'index']);
